4729ba85941ae7209aa1e361b8a23158c2a3dc72
vagrant <michele.dn@live.com>
2016-09-26 19:02:30 +0000

    Timestamps

--- app/Models/PlayerEvent.php
+++ app/Models/PlayerEvent.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class PlayerEvent extends Model
 {
+    public $timestamps = false;
+
     protected $fillable = [
         'attending',
         'comment',
