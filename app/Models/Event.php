6834a36f61ada7432bd6ccaa2d30d84d4dfacc1a
vagrant <michele.dn@live.com>
2016-09-26 18:54:12 +0000

    Add Models

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    //
+    protected $fillable = [
+        'description',
+    ];
+
+    protected $dates = ['event_datetime'];
 }
--- app/Models/EventType.php
+++ app/Models/EventType.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class EventType extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+    ];
+
+    public $timestsamp = false;
 }
